6a81306b5ede7764583a9e89813a267f5926580c
jdeslangles <julien.deslangles@gmail.com>
2013-06-26 12:14:51 +0100

    using params

--- hi.rb
+++ hi.rb
@@ -1,9 +1,16 @@
 require 'pry'
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/contrib/all'
 
 get '/hi' do
   "Hello, WDI!"
 end
 
+get '/' do                  # a / on its own is the root of your site
+  "this is the homepage"
+end
 
+get '/name/:first' do
+  return "hello, #{params[:first]}"
+  "oioi" # --> Careful!!! the last thing is what will be returned UNLESS you've specified what you want to return.
+end
